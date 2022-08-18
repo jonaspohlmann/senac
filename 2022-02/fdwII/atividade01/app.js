6b085d501587889f7b702449b1a9444102e4a73e
Jonas Pohlmann Araujo <jonasparaujo852@gmail.com>
2022-08-18 20:50:56 -0300

    rotas com express

---
+++ 2022-02/fdwII/atividade01/app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+const port = 3000
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+
+//GET /produtos
+app.get('/produtos', function (req, res) {
+    res.send('GET /produtos');
+});
+
+//GET /produtos/:id
+app.get('/produtos/:id', function (req, res) {
+    res.send('GET /produtos/:id');
+});
+
+//POST /produtos
+app.post('/produtos', function (req, res) {
+    res.send('POST /produtos');
+});
+
+//PUT /produtos/:id
+app.put('/produtos/:id', function (req, res) {
+    res.send('PUT /produtos/:id');
+});
+
+//DELETE /produtos/:id
+app.delete('/produtos/:id', function (req, res) {
+    res.send('DELETE /produtos/:id');
+});
+  
