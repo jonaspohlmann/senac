081570cef78cf9ed372ad5df6b2024a4f564e41f
Jonas Pohlmann Araujo <jonasparaujo852@gmail.com>
2022-08-18 21:52:25 -0300

    atividade 1

--- 2022-02/fdwII/atividade01/app.js
+++ 2022-02/fdwII/atividade01/app.js
@@ -2,32 +2,42 @@ var express = require('express');
 var app = express();
 const port = 3000
 
+let listaProdutos = [];
+let geradorId = 1;
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
 
+app.use(express.json())
+
 //GET /produtos
 app.get('/produtos', function (req, res) {
-    res.send('GET /produtos');
+    res.send(listaProdutos);
 });
 
 //GET /produtos/:id
 app.get('/produtos/:id', function (req, res) {
-    res.send('GET /produtos/:id');
+    res.send('Buscando o produto '+req.params.id);
 });
 
 //POST /produtos
 app.post('/produtos', function (req, res) {
-    res.send('POST /produtos');
+    let nome = req.body.nome;
+    let preco = req.body.preco;
+
+    listaProdutos.push(geradorId, nome, preco);
+    res.send('Cadastrando produto '+ nome);
+
+    geradorId++;
 });
 
 //PUT /produtos/:id
 app.put('/produtos/:id', function (req, res) {
-    res.send('PUT /produtos/:id');
+    res.send('Atualizando o produto '+req.params.id)
 });
 
 //DELETE /produtos/:id
 app.delete('/produtos/:id', function (req, res) {
-    res.send('DELETE /produtos/:id');
+    res.send('Removendo o produto '+req.params.id)
 });
-  
